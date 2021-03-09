b4e6d5678218eadc77d97e367d87f14336be5599
Muhammad Arya Prayoga <arya.prayoga222.ap@gmail.com>
2021-03-09 21:06:20 +0700

    Operation : remove word from dictionary

---
+++ interface/edit_dict/remove_word.cc
@@ -0,0 +1,40 @@
+/*
+* M. ARYA PRAYOGA
+* SUNDAY, 7 MARCH 2021
+*
+* Operation : remove word from dictionary
+*/
+
+#include "my_dictionary/interface/edit_dict/remove_word.h"
+
+#include <iostream>
+#include <string>
+
+#include "my_dictionary/dictionary.h"
+#include "my_dictionary/error.h"
+#include "my_dictionary/interface/auxiliary.h"
+
+namespace my_dictionary {
+
+// Prompt user to enter word-name
+// Remove that word from dict
+void RemoveWord(Dictionary& dict) {
+  ClearScreen();
+  std::cout << "Removing word from dictionary \"" << dict.get_name() << "\"";
+
+  // Prompt user to input word-name
+  std::cout << "\n\nEnter word-name : ";
+  std::string word_name;
+  std::cin.ignore();  // Ignore previous newline
+  std::getline(std::cin, word_name);
+  if (!std::cin)
+    throw BadInput("Error while reading input from user", std::cin);
+
+  dict.RemoveWord(word_name);     // Remove word from dict
+
+  // Report removed word
+  std::cout << "\n\nWord \"" << word_name << "\" removed";
+  WaitForButton();
+}
+
+} // my_dictionary
