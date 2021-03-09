92d216623d287e9c57977928a5eb8c1810878709
Muhammad Arya Prayoga <arya.prayoga222.ap@gmail.com>
2021-03-09 21:07:06 +0700

    Operation : remove meaning from spesific word

---
+++ interface/edit_word/remove_meaning.h
@@ -0,0 +1,19 @@
+/*
+* M. ARYA PRAYOGA
+* TUESDAY, 9 MARCH 2021
+*
+* Operation : remove meanings for spesific word
+*/
+
+#ifndef MY_DICTIONARY_INTERFACE_EDIT_WORD_REMOVE_MEANING_H_
+#define MY_DICTIONARY_INTERFACE_EDIT_WORD_REMOVE_MEANING_H_
+
+#include "my_dictionary/word.h"
+
+namespace my_dictionary {
+
+// Remove meanings from word w
+void RemoveMeaning(Word& w);
+
+}
+#endif // MY_DICTIONARY_INTERFACE_EDIT_WORD_REMOVE_MEANING_H_
