25b555cf4909e34367c02127e5d83ca004a3a236
Muhammad Arya Prayoga <arya.prayoga222.ap@gmail.com>
2021-03-09 21:08:31 +0700

    Operation : change classification of spesific word

---
+++ interface/edit_word/change_class.h
@@ -0,0 +1,20 @@
+/*
+* M. ARYA PRAYOGA
+* TUESDAY, 9 MARCH 2021
+*
+* Operation : change classification for spesific word
+*/
+
+#ifndef MY_DICTIONARY_INTERFACE_EDIT_WORD_CHANGE_CLASS_H_
+#define MY_DICTIONARY_INTERFACE_EDIT_WORD_CHANGE_CLASS_H_
+
+#include "my_dictionary/word.h"
+
+namespace my_dictionary {
+
+// Change the classification of word w
+void ChangeClass(Word& w);
+
+} // my_dictionary
+
+#endif //MY_DICTIONARY_INTERFACE_EDIT_WORD_CHANGE_CLASS_H_
